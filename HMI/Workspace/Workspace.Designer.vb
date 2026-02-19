65cf5249e4dd402edb3ab550a8bfc2f0b2076605
YANAi <github@secure.ta-yan.ai>
2026-02-19 19:17:32 +0900

    [Add] 初期登録　Workspace画面を追加

---
+++ HMI/Workspace/Workspace.Designer.vb
@@ -0,0 +1,144 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Workspace
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        pnlDBList = New Panel()
+        treeDBList = New TreeView()
+        splList = New Splitter()
+        plnTableList = New Panel()
+        lstTableList = New ListView()
+        plnTableSearch = New TableLayoutPanel()
+        txtTableSearch = New TextBox()
+        btnTableSearch = New Button()
+        pnlDBList.SuspendLayout()
+        plnTableList.SuspendLayout()
+        plnTableSearch.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' pnlDBList
+        ' 
+        pnlDBList.BackColor = SystemColors.Control
+        pnlDBList.Controls.Add(treeDBList)
+        pnlDBList.Dock = DockStyle.Left
+        pnlDBList.Location = New Point(0, 0)
+        pnlDBList.Name = "pnlDBList"
+        pnlDBList.Size = New Size(300, 811)
+        pnlDBList.TabIndex = 0
+        ' 
+        ' treeDBList
+        ' 
+        treeDBList.Dock = DockStyle.Fill
+        treeDBList.Location = New Point(0, 0)
+        treeDBList.Name = "treeDBList"
+        treeDBList.Size = New Size(300, 811)
+        treeDBList.TabIndex = 0
+        ' 
+        ' splList
+        ' 
+        splList.BackColor = SystemColors.ButtonShadow
+        splList.Location = New Point(300, 0)
+        splList.Name = "splList"
+        splList.Size = New Size(15, 811)
+        splList.TabIndex = 1
+        splList.TabStop = False
+        ' 
+        ' plnTableList
+        ' 
+        plnTableList.Controls.Add(lstTableList)
+        plnTableList.Controls.Add(plnTableSearch)
+        plnTableList.Dock = DockStyle.Fill
+        plnTableList.Location = New Point(315, 0)
+        plnTableList.Name = "plnTableList"
+        plnTableList.Size = New Size(835, 811)
+        plnTableList.TabIndex = 2
+        ' 
+        ' lstTableList
+        ' 
+        lstTableList.Dock = DockStyle.Fill
+        lstTableList.Location = New Point(0, 37)
+        lstTableList.Name = "lstTableList"
+        lstTableList.Size = New Size(835, 774)
+        lstTableList.TabIndex = 2
+        lstTableList.UseCompatibleStateImageBehavior = False
+        ' 
+        ' plnTableSearch
+        ' 
+        plnTableSearch.AutoSize = True
+        plnTableSearch.ColumnCount = 2
+        plnTableSearch.ColumnStyles.Add(New ColumnStyle(SizeType.Absolute, 100F))
+        plnTableSearch.ColumnStyles.Add(New ColumnStyle())
+        plnTableSearch.Controls.Add(txtTableSearch, 1, 0)
+        plnTableSearch.Controls.Add(btnTableSearch, 0, 0)
+        plnTableSearch.Dock = DockStyle.Top
+        plnTableSearch.Location = New Point(0, 0)
+        plnTableSearch.Name = "plnTableSearch"
+        plnTableSearch.RowCount = 1
+        plnTableSearch.RowStyles.Add(New RowStyle(SizeType.Percent, 100F))
+        plnTableSearch.Size = New Size(835, 37)
+        plnTableSearch.TabIndex = 1
+        ' 
+        ' txtTableSearch
+        ' 
+        txtTableSearch.Dock = DockStyle.Fill
+        txtTableSearch.Location = New Point(103, 3)
+        txtTableSearch.Name = "txtTableSearch"
+        txtTableSearch.Size = New Size(729, 31)
+        txtTableSearch.TabIndex = 2
+        ' 
+        ' btnTableSearch
+        ' 
+        btnTableSearch.Dock = DockStyle.Fill
+        btnTableSearch.Location = New Point(3, 3)
+        btnTableSearch.Name = "btnTableSearch"
+        btnTableSearch.Size = New Size(94, 31)
+        btnTableSearch.TabIndex = 0
+        btnTableSearch.Text = "検索"
+        btnTableSearch.UseVisualStyleBackColor = True
+        ' 
+        ' Workspace
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1150, 811)
+        Controls.Add(plnTableList)
+        Controls.Add(splList)
+        Controls.Add(pnlDBList)
+        Name = "Workspace"
+        Text = "Workspace"
+        pnlDBList.ResumeLayout(False)
+        plnTableList.ResumeLayout(False)
+        plnTableList.PerformLayout()
+        plnTableSearch.ResumeLayout(False)
+        plnTableSearch.PerformLayout()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents pnlDBList As Panel
+    Friend WithEvents splList As Splitter
+    Friend WithEvents plnTableList As Panel
+    Friend WithEvents plnTableSearch As TableLayoutPanel
+    Friend WithEvents btnTableSearch As Button
+    Friend WithEvents txtTableSearch As TextBox
+    Friend WithEvents treeDBList As TreeView
+    Friend WithEvents lstTableList As ListView
+End Class
---
+++ HMI/Workspace/Workspace.vb
@@ -0,0 +1,44 @@
+﻿Public Class Workspace
+    Inherits Form
+
+    Private DumpFilePath As String
+    Private WorkspacePath As String
+
+    ' 引数ありコンストラクタ
+    Public Sub New(value1 As String, value2 As String)
+        InitializeComponent()
+        DumpFilePath = value1
+        WorkspacePath = value2
+    End Sub
+
+
+    Private Sub Workspace_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'ダンプファイルの解析を実行する
+        AnalyzeLogic.AnalyzeDumpFile(DumpFilePath)
+
+        'テストデータを追加する
+        AddTestDataToTreeDBList()
+    End Sub
+
+    Private Sub AddTestDataToTreeDBList()
+        'ルートノードを追加
+        Dim rootNode As TreeNode = treeDBList.Nodes.Add("Database")
+
+        'スキーマノードを追加
+        Dim schemaNode1 As TreeNode = rootNode.Nodes.Add("SCHEMA1")
+        Dim schemaNode2 As TreeNode = rootNode.Nodes.Add("SCHEMA2")
+
+        'テーブルノードをSCHEMA1に追加
+        schemaNode1.Nodes.Add("EMP_TABLE")
+        schemaNode1.Nodes.Add("DEPT_TABLE")
+        schemaNode1.Nodes.Add("SALARY_TABLE")
+
+        'テーブルノードをSCHEMA2に追加
+        schemaNode2.Nodes.Add("USERS_TABLE")
+        schemaNode2.Nodes.Add("PRODUCTS_TABLE")
+        schemaNode2.Nodes.Add("ORDERS_TABLE")
+
+        'ルートノードを展開
+        rootNode.Expand()
+    End Sub
+End Class

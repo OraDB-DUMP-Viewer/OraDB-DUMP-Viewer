767bc236f8929b0334ad17921afb332de8ad53b8
YANAi <github@secure.ta-yan.ai>
2026-02-21 22:13:30 +0900

    [Update] 画面レイアウト修正

--- HMI/TablePreview/AdvancedSearchForm.Designer.vb
+++ HMI/TablePreview/AdvancedSearchForm.Designer.vb
@@ -22,153 +22,163 @@ Partial Class AdvancedSearchForm
     'コード エディターを使って変更しないでください。
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
-        Me.mainPanel = New System.Windows.Forms.TableLayoutPanel()
-        Me.scrollPanel = New System.Windows.Forms.Panel()
-        Me.flowLayoutPanel = New System.Windows.Forms.FlowLayoutPanel()
-        Me.buttonPanel = New System.Windows.Forms.Panel()
-        Me.buttonAdd = New System.Windows.Forms.Button()
-        Me.buttonClear = New System.Windows.Forms.Button()
-        Me.buttonSearch = New System.Windows.Forms.Button()
-        Me.buttonCancel = New System.Windows.Forms.Button()
-        Me.templateConditionRow = New SearchConditionRow(New List(Of String)())
-        Me.templateLogicalPanel = New System.Windows.Forms.Panel()
-        Me.mainPanel.SuspendLayout()
-        Me.scrollPanel.SuspendLayout()
-        Me.buttonPanel.SuspendLayout()
-        Me.SuspendLayout()
-        '
-        'mainPanel
-        '
-        Me.mainPanel.ColumnCount = 1
-        Me.mainPanel.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
-        Me.mainPanel.Controls.Add(Me.scrollPanel, 0, 0)
-        Me.mainPanel.Controls.Add(Me.buttonPanel, 0, 1)
-        Me.mainPanel.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.mainPanel.Location = New System.Drawing.Point(0, 0)
-        Me.mainPanel.Name = "mainPanel"
-        Me.mainPanel.RowCount = 2
-        Me.mainPanel.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
-        Me.mainPanel.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 60.0!))
-        Me.mainPanel.Size = New System.Drawing.Size(900, 600)
-        Me.mainPanel.TabIndex = 0
-        '
-        'scrollPanel
-        '
-        Me.scrollPanel.AutoScroll = True
-        Me.scrollPanel.Controls.Add(Me.flowLayoutPanel)
-        Me.scrollPanel.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.scrollPanel.Location = New System.Drawing.Point(3, 3)
-        Me.scrollPanel.Name = "scrollPanel"
-        Me.scrollPanel.Size = New System.Drawing.Size(894, 534)
-        Me.scrollPanel.TabIndex = 0
-        '
-        'flowLayoutPanel
-        '
-        Me.flowLayoutPanel.AutoSize = True
-        Me.flowLayoutPanel.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
-        Me.flowLayoutPanel.BackColor = System.Drawing.SystemColors.Control
-        Me.flowLayoutPanel.FlowDirection = System.Windows.Forms.FlowDirection.TopDown
-        Me.flowLayoutPanel.Location = New System.Drawing.Point(10, 10)
-        Me.flowLayoutPanel.Name = "flowLayoutPanel"
-        Me.flowLayoutPanel.Padding = New System.Windows.Forms.Padding(10)
-        Me.flowLayoutPanel.Size = New System.Drawing.Size(0, 20)
-        Me.flowLayoutPanel.TabIndex = 0
-        '
-        'buttonPanel
-        '
-        Me.buttonPanel.Controls.Add(Me.buttonAdd)
-        Me.buttonPanel.Controls.Add(Me.buttonClear)
-        Me.buttonPanel.Controls.Add(Me.buttonSearch)
-        Me.buttonPanel.Controls.Add(Me.buttonCancel)
-        Me.buttonPanel.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.buttonPanel.Location = New System.Drawing.Point(3, 543)
-        Me.buttonPanel.Name = "buttonPanel"
-        Me.buttonPanel.Size = New System.Drawing.Size(894, 54)
-        Me.buttonPanel.TabIndex = 1
-        '
-        'buttonAdd
-        '
-        Me.buttonAdd.Location = New System.Drawing.Point(10, 10)
-        Me.buttonAdd.Name = "buttonAdd"
-        Me.buttonAdd.Size = New System.Drawing.Size(100, 40)
-        Me.buttonAdd.TabIndex = 0
-        Me.buttonAdd.Text = "条件を追加"
-        Me.buttonAdd.UseVisualStyleBackColor = True
-        '
-        'buttonClear
-        '
-        Me.buttonClear.Location = New System.Drawing.Point(120, 10)
-        Me.buttonClear.Name = "buttonClear"
-        Me.buttonClear.Size = New System.Drawing.Size(100, 40)
-        Me.buttonClear.TabIndex = 1
-        Me.buttonClear.Text = "クリア"
-        Me.buttonClear.UseVisualStyleBackColor = True
-        '
-        'buttonSearch
-        '
-        Me.buttonSearch.BackColor = System.Drawing.SystemColors.Highlight
-        Me.buttonSearch.DialogResult = System.Windows.Forms.DialogResult.OK
-        Me.buttonSearch.ForeColor = System.Drawing.Color.White
-        Me.buttonSearch.Location = New System.Drawing.Point(230, 10)
-        Me.buttonSearch.Name = "buttonSearch"
-        Me.buttonSearch.Size = New System.Drawing.Size(100, 40)
-        Me.buttonSearch.TabIndex = 2
-        Me.buttonSearch.Text = "検索"
-        Me.buttonSearch.UseVisualStyleBackColor = False
-        '
-        'buttonCancel
-        '
-        Me.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel
-        Me.buttonCancel.Location = New System.Drawing.Point(340, 10)
-        Me.buttonCancel.Name = "buttonCancel"
-        Me.buttonCancel.Size = New System.Drawing.Size(100, 40)
-        Me.buttonCancel.TabIndex = 3
-        Me.buttonCancel.Text = "キャンセル"
-        Me.buttonCancel.UseVisualStyleBackColor = True
-        '
-        'templateConditionRow
-        '
-        Me.templateConditionRow.AutoSize = True
-        Me.templateConditionRow.BackColor = System.Drawing.SystemColors.Control
-        Me.templateConditionRow.Height = 40
-        Me.templateConditionRow.Name = "templateConditionRow"
-        Me.templateConditionRow.Padding = New System.Windows.Forms.Padding(5)
-        Me.templateConditionRow.Size = New System.Drawing.Size(720, 40)
-        Me.templateConditionRow.TabIndex = 0
-        Me.templateConditionRow.Visible = False
-        '
-        'templateLogicalPanel
-        '
-        Me.templateLogicalPanel.AutoSize = True
-        Me.templateLogicalPanel.BackColor = System.Drawing.SystemColors.Control
-        Me.templateLogicalPanel.Height = 40
-        Me.templateLogicalPanel.Name = "templateLogicalPanel"
-        Me.templateLogicalPanel.Size = New System.Drawing.Size(0, 0)
-        Me.templateLogicalPanel.TabIndex = 0
-        Me.templateLogicalPanel.Visible = False
-        '
-        'AdvancedSearchForm
-        '
-        Me.AcceptButton = Me.buttonSearch
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.CancelButton = Me.buttonCancel
-        Me.ClientSize = New System.Drawing.Size(900, 600)
-        Me.Controls.Add(Me.mainPanel)
-        Me.Controls.Add(Me.templateConditionRow)
-        Me.Controls.Add(Me.templateLogicalPanel)
-        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
-        Me.MaximizeBox = False
-        Me.MinimizeBox = False
-        Me.Name = "AdvancedSearchForm"
-        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
-        Me.Text = "高度な検索"
-        Me.mainPanel.ResumeLayout(False)
-        Me.scrollPanel.ResumeLayout(False)
-        Me.scrollPanel.PerformLayout()
-        Me.buttonPanel.ResumeLayout(False)
-        Me.ResumeLayout(False)
+        mainPanel = New TableLayoutPanel()
+        scrollPanel = New Panel()
+        flowLayoutPanel = New FlowLayoutPanel()
+        buttonPanel = New Panel()
+        buttonAdd = New Button()
+        buttonClear = New Button()
+        buttonSearch = New Button()
+        buttonCancel = New Button()
+        templateLogicalPanel = New Panel()
+        mainPanel.SuspendLayout()
+        scrollPanel.SuspendLayout()
+        buttonPanel.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' mainPanel
+        ' 
+        mainPanel.ColumnCount = 1
+        mainPanel.ColumnStyles.Add(New ColumnStyle(SizeType.Percent, 100F))
+        mainPanel.Controls.Add(scrollPanel, 0, 0)
+        mainPanel.Controls.Add(buttonPanel, 0, 1)
+        mainPanel.Dock = DockStyle.Fill
+        mainPanel.Location = New Point(0, 0)
+        mainPanel.Margin = New Padding(5, 6, 5, 6)
+        mainPanel.Name = "mainPanel"
+        mainPanel.RowCount = 2
+        mainPanel.RowStyles.Add(New RowStyle(SizeType.Percent, 100F))
+        mainPanel.RowStyles.Add(New RowStyle(SizeType.Absolute, 115F))
+        mainPanel.Size = New Size(1500, 1154)
+        mainPanel.TabIndex = 0
+        ' 
+        ' scrollPanel
+        ' 
+        scrollPanel.AutoScroll = True
+        scrollPanel.Controls.Add(flowLayoutPanel)
+        scrollPanel.Dock = DockStyle.Fill
+        scrollPanel.Location = New Point(5, 6)
+        scrollPanel.Margin = New Padding(5, 6, 5, 6)
+        scrollPanel.Name = "scrollPanel"
+        scrollPanel.Size = New Size(1490, 1027)
+        scrollPanel.TabIndex = 0
+        ' 
+        ' flowLayoutPanel
+        ' 
+        flowLayoutPanel.AutoSize = True
+        flowLayoutPanel.AutoSizeMode = AutoSizeMode.GrowAndShrink
+        flowLayoutPanel.BackColor = SystemColors.Control
+        flowLayoutPanel.FlowDirection = FlowDirection.TopDown
+        flowLayoutPanel.Location = New Point(17, 19)
+        flowLayoutPanel.Margin = New Padding(5, 6, 5, 6)
+        flowLayoutPanel.Name = "flowLayoutPanel"
+        flowLayoutPanel.Padding = New Padding(17, 19, 17, 19)
+        flowLayoutPanel.Size = New Size(34, 38)
+        flowLayoutPanel.TabIndex = 0
+        ' 
+        ' buttonPanel
+        ' 
+        buttonPanel.Controls.Add(buttonSearch)
+        buttonPanel.Controls.Add(buttonCancel)
+        buttonPanel.Controls.Add(buttonClear)
+        buttonPanel.Controls.Add(buttonAdd)
+        buttonPanel.Dock = DockStyle.Fill
+        buttonPanel.Location = New Point(5, 1045)
+        buttonPanel.Margin = New Padding(5, 6, 5, 6)
+        buttonPanel.Name = "buttonPanel"
+        buttonPanel.Size = New Size(1490, 103)
+        buttonPanel.TabIndex = 1
+        ' 
+        ' buttonAdd
+        ' 
+        buttonAdd.Location = New Point(200, 19)
+        buttonAdd.Margin = New Padding(5, 6, 5, 6)
+        buttonAdd.Name = "buttonAdd"
+        buttonAdd.Size = New Size(167, 77)
+        buttonAdd.TabIndex = 0
+        buttonAdd.Text = "条件を追加"
+        buttonAdd.UseVisualStyleBackColor = True
+        ' 
+        ' buttonClear
+        ' 
+        buttonClear.Location = New Point(383, 19)
+        buttonClear.Margin = New Padding(5, 6, 5, 6)
+        buttonClear.Name = "buttonClear"
+        buttonClear.Size = New Size(167, 77)
+        buttonClear.TabIndex = 1
+        buttonClear.Text = "クリア"
+        buttonClear.UseVisualStyleBackColor = True
+        ' 
+        ' buttonSearch
+        ' 
+        buttonSearch.BackColor = SystemColors.Highlight
+        buttonSearch.DialogResult = DialogResult.OK
+        buttonSearch.ForeColor = Color.White
+        buttonSearch.Location = New Point(17, 19)
+        buttonSearch.Margin = New Padding(5, 6, 5, 6)
+        buttonSearch.Name = "buttonSearch"
+        buttonSearch.Size = New Size(167, 77)
+        buttonSearch.TabIndex = 2
+        buttonSearch.Text = "検索"
+        buttonSearch.UseVisualStyleBackColor = False
+        ' 
+        ' buttonCancel
+        ' 
+        buttonCancel.DialogResult = DialogResult.Cancel
+        buttonCancel.Location = New Point(567, 19)
+        buttonCancel.Margin = New Padding(5, 6, 5, 6)
+        buttonCancel.Name = "buttonCancel"
+        buttonCancel.Size = New Size(167, 77)
+        buttonCancel.TabIndex = 3
+        buttonCancel.Text = "キャンセル"
+        buttonCancel.UseVisualStyleBackColor = True
+        ' 
+        ' templateConditionRow
+        ' 
+        templateConditionRow.AutoSize = True
+        templateConditionRow.BackColor = SystemColors.Control
+        templateConditionRow.Location = New Point(0, 0)
+        templateConditionRow.Margin = New Padding(5, 6, 5, 6)
+        templateConditionRow.Name = "templateConditionRow"
+        templateConditionRow.Padding = New Padding(8, 10, 8, 10)
+        templateConditionRow.Size = New Size(1200, 77)
+        templateConditionRow.TabIndex = 0
+        templateConditionRow.Visible = False
+        ' 
+        ' templateLogicalPanel
+        ' 
+        templateLogicalPanel.AutoSize = True
+        templateLogicalPanel.BackColor = SystemColors.Control
+        templateLogicalPanel.Location = New Point(0, 0)
+        templateLogicalPanel.Margin = New Padding(5, 6, 5, 6)
+        templateLogicalPanel.Name = "templateLogicalPanel"
+        templateLogicalPanel.Size = New Size(0, 0)
+        templateLogicalPanel.TabIndex = 0
+        templateLogicalPanel.Visible = False
+        ' 
+        ' AdvancedSearchForm
+        ' 
+        AcceptButton = buttonSearch
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        CancelButton = buttonCancel
+        ClientSize = New Size(1500, 1154)
+        Controls.Add(mainPanel)
+        Controls.Add(templateConditionRow)
+        Controls.Add(templateLogicalPanel)
+        FormBorderStyle = FormBorderStyle.FixedDialog
+        Margin = New Padding(5, 6, 5, 6)
+        MaximizeBox = False
+        MinimizeBox = False
+        Name = "AdvancedSearchForm"
+        StartPosition = FormStartPosition.CenterParent
+        Text = "高度な検索"
+        mainPanel.ResumeLayout(False)
+        scrollPanel.ResumeLayout(False)
+        scrollPanel.PerformLayout()
+        buttonPanel.ResumeLayout(False)
+        ResumeLayout(False)
+        PerformLayout()
 
     End Sub
 
